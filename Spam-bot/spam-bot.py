e37c9a9fc49b0750f7ebf94e17a2e0f1eaf632d0
anannya03 <40665106+anannya03@users.noreply.github.com>
2020-07-06 11:46:01 +0530

    Update spam-bot.py

--- Spam-bot/spam-bot.py
+++ Spam-bot/spam-bot.py
@@ -1,10 +1,14 @@
 import pyautogui as pg
 import time
+
+#initialise the pointer
 i = 0
+#program sleeps/waits for 5 seconds d=before executing further
 time.sleep(5)
+
 while i<20:
-    pg.click(1405,984)
-    pg.typewrite("Hi ",interval=0.001)
+    pg.click(1405,984) #mouse pointer clicks at the given position
+    pg.typewrite("HI! ",interval=0.001) #Types the message 'HI!' at the interval of 0.001 seconds
     pg.click(1883,984)
-    pg.press('enter')
-    i = i+1
+    pg.press('enter') #Automatically presses the enter key
+    i = i+1 #pointer is incremented
